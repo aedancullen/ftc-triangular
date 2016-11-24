d47ee9465127d19d18a37f086e9cc8a4613a42e0
Aedan Cullen <aedancullen@gmail.com>
2016-11-24 10:39:48 -0500

    Adding to VisionProcessor

--- src/main/java/io/github/aedancullen/triangular/VisionProcessor.java
+++ src/main/java/io/github/aedancullen/triangular/VisionProcessor.java
@@ -1,15 +1,12 @@
 package io.github.aedancullen.triangular;
 
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceView;
-import android.view.ViewGroup;
 import android.widget.LinearLayout;
 
-import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcontroller.internal.FtcRobotControllerActivity;
 import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase;
 import org.opencv.android.JavaCameraView;
@@ -21,20 +18,26 @@ import io.github.cn04.triangular.R;
 
 public class VisionProcessor implements CameraBridgeViewBase.CvCameraViewListener2 {
 
-    private static FtcRobotControllerActivity robotControllerActivity;
     private static final String TAG = "VisionProcessor";
     private CameraBridgeViewBase mOpenCvCameraView;
+    private Context appContext;
+    private Activity activity;
 
-    public static VisionProcessor fromHardwareMap(HardwareMap hardwareMap) {
-        try {
-            return ((FtcRobotControllerActivity) hardwareMap.appContext).visionProcessor;
-        }
-        catch (Exception e) {
-            throw new UnsupportedOperationException("The modified FtcRobotControllerActivity with Triangular support is not running");
-        }
+    //
+    public static VisionProcessor newVisionProcessor;
+
+    private static VisionProcessor start(final Context context) {
+        Runnable starter = new Runnable() {
+            public void run() {
+                VisionProcessor.newVisionProcessor =  new VisionProcessor(context);
+            }
+        };
+        ((Activity)context).runOnUiThread(starter);
+        return newVisionProcessor;
     }
+    //
 
-    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(robotControllerActivity) {
+    private BaseLoaderCallback mLoaderCallback = new BaseLoaderCallback(activity) {
         @Override
         public void onManagerConnected(int status) {
             switch (status) {
@@ -51,15 +54,17 @@ public class VisionProcessor implements CameraBridgeViewBase.CvCameraViewListene
         }
     };
 
-    public VisionProcessor() {
+    public VisionProcessor(Context context) {
+        appContext = context;
+        activity = (Activity)context;
         Log.i(TAG, "Instantiated new " + this.getClass());
     }
 
     public void onCreate(Bundle savedInstanceState) {
         Log.i(TAG, "called onCreate");
 
-        LinearLayout cameraViewParent = (LinearLayout) robotControllerActivity.findViewById(R.id.cameraMonitorViewId);
-        mOpenCvCameraView = new JavaCameraView(robotControllerActivity, 0);
+        LinearLayout cameraViewParent = (LinearLayout) activity.findViewById(R.id.cameraMonitorViewId);
+        mOpenCvCameraView = new JavaCameraView(activity, 0);
         cameraViewParent.addView(mOpenCvCameraView, new LinearLayout.LayoutParams(LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.FILL_PARENT));
 
         mOpenCvCameraView.setVisibility(SurfaceView.VISIBLE);
@@ -78,7 +83,7 @@ public class VisionProcessor implements CameraBridgeViewBase.CvCameraViewListene
     {
         if (!OpenCVLoader.initDebug()) {
             Log.d(TAG, "Internal OpenCV library not found. Using OpenCV Manager for initialization of 2.4.13");
-            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_13, robotControllerActivity, mLoaderCallback);
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_13, activity, mLoaderCallback);
         } else {
             Log.d(TAG, "OpenCV library found inside package. Using it!");
             mLoaderCallback.onManagerConnected(LoaderCallbackInterface.SUCCESS);
