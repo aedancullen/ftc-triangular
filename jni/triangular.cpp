c8031a4327764017024763c23000fa09c908d124
Aedan Cullen <aedancullen@gmail.com>
2016-11-26 15:10:33 -0500

    Add triangular cpp and h

---
+++ jni/triangular.cpp
@@ -0,0 +1,5 @@
+//
+// Created by aedan on 11/26/16.
+//
+
+#include "triangular.h"
---
+++ jni/triangular.h
@@ -0,0 +1,8 @@
+//
+// Created by aedan on 11/26/16.
+//
+
+#ifndef TRIANGULAR_H
+#define TRIANGULAR_H
+
+#endif //TRIANGULAR_H
