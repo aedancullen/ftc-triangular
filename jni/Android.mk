fd52846a616914dcc88e2852e88037e6472e55b8
Aedan Cullen <aedancullen@gmail.com>
2016-11-26 14:25:16 -0500

    Reorganization for integration of NDK

---
+++ jni/Android.mk
@@ -0,0 +1,19 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+ifdef OPENCV_ANDROID_SDK
+  ifneq ("","$(wildcard $(OPENCV_ANDROID_SDK)/OpenCV.mk)")
+    include ${OPENCV_ANDROID_SDK}/OpenCV.mk
+  else
+    include ${OPENCV_ANDROID_SDK}/sdk/native/jni/OpenCV.mk
+  endif
+else
+  include ../../OpenCV-android-sdk/sdk/native/jni/OpenCV.mk
+endif
+
+LOCAL_MODULE    := triangular-native
+LOCAL_SRC_FILES := mosse.cpp
+LOCAL_LDLIBS +=  -llog -ldl
+
+include $(BUILD_SHARED_LIBRARY)
---
+++ jni/mosse.h
@@ -0,0 +1,8 @@
+//
+// Created by aedan on 11/26/16.
+//
+
+#ifndef TRIANGULAR_MOSSE_H
+#define TRIANGULAR_MOSSE_H
+
+#endif //TRIANGULAR_MOSSE_H
