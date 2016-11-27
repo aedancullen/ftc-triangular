4b999490a59a38aff45de7e62b3f8f2ed4176602
Aedan Cullen <aedancullen@gmail.com>
2016-11-26 21:54:16 -0500

    rename jni.c to triangular.cpp

--- jni/Android.mk
+++ jni/Android.mk
@@ -13,7 +13,7 @@ else
 endif
 
 LOCAL_MODULE    := triangular-native
-LOCAL_SRC_FILES := mosse.cpp jni.c
+LOCAL_SRC_FILES := awareness.cpp
 LOCAL_LDLIBS +=  -llog -ldl
 
 include $(BUILD_SHARED_LIBRARY)
