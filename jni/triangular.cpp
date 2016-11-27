df4f36f1eb2c19797da56a32ff79cbd44d314439
Aedan Cullen <aedancullen@gmail.com>
2016-11-27 16:53:06 -0500

    Add native method calls

--- jni/triangular.cpp
+++ jni/triangular.cpp
@@ -2,3 +2,18 @@
 #include <jni.h>
 #include <mosse.cpp>
 
+Tracker tracker;
+
+
+extern "C" JNIEXPORT jint JNICALL Java_io_github_aedancullen_triangular_system_NativePart_test(JNIEnv*, jobject, jint testValue) {
+    return testValue * 2;
+}
+
+
+extern "C" JNIEXPORT void JNICALL Java_io_github_aedancullen_triangular_system_NativePart_InitTracker(JNIEnv*, jobject, jlong matAddr, jint x, jint y, jint w, jint h) {
+    tracker.InitTracker(*(Mat*)matAddr, *new Rect(x, y, w, h));
+}
+
+extern "C" JNIEXPORT void JNICALL Java_io_github_aedancullen_triangular_system_NativePart_Track(JNIEnv*, jobject, jlong matAddr) {
+    tracker.Track(*(Mat*)matAddr);
+}
--- src/main/java/io/github/aedancullen/triangular/system/NativePart.java
+++ src/main/java/io/github/aedancullen/triangular/system/NativePart.java
@@ -5,4 +5,12 @@ package io.github.aedancullen.triangular.system;
  */
 
 public class NativePart {
+
+    public native int test(int testValue);
+
+
+    public native void InitTracker(long matAddr, int x, int y, int w, int h);
+
+    public native void Track(long matAddr);
+
 }
