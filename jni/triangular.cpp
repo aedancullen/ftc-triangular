d3d912e174deae7a098d09f57260847b0112aff2
Aedan Cullen <aedancullen@gmail.com>
2016-12-09 15:18:55 -0500

    Fix locations

--- jni/triangular.cpp
+++ jni/triangular.cpp
@@ -5,24 +5,24 @@
 Tracker tracker;
 
 
-extern "C" JNIEXPORT jint JNICALL Java_io_github_aedancullen_triangular_system_NativePart_test(JNIEnv*, jobject, jint testValue) {
+extern "C" JNIEXPORT jint JNICALL Java_io_github_aedancullen_triangular_NativePart_test(JNIEnv*, jobject, jint testValue) {
     return testValue * 2;
 }
 
 
-extern "C" JNIEXPORT void JNICALL Java_io_github_aedancullen_triangular_system_NativePart_InitTracker(JNIEnv*, jobject, jlong matAddr, jint x, jint y, jint w, jint h) {
+extern "C" JNIEXPORT void JNICALL Java_io_github_aedancullen_triangular_NativePart_InitTracker(JNIEnv*, jobject, jlong matAddr, jint x, jint y, jint w, jint h) {
     tracker.InitTracker(*(Mat*)matAddr, *new Rect(x, y, w, h));
 }
 
-extern "C" JNIEXPORT void JNICALL Java_io_github_aedancullen_triangular_system_NativePart_Track(JNIEnv*, jobject, jlong matAddr) {
+extern "C" JNIEXPORT void JNICALL Java_io_github_aedancullen_triangular_NativePart_Track(JNIEnv*, jobject, jlong matAddr) {
     tracker.Track(*(Mat*)matAddr);
 }
 
-extern "C" JNIEXPORT jboolean JNICALL Java_io_github_aedancullen_triangular_system_NativePart_isInitialized(JNIEnv*, jobject) {
+extern "C" JNIEXPORT jboolean JNICALL Java_io_github_aedancullen_triangular_NativePart_isInitialized(JNIEnv*, jobject) {
     return tracker.isInitialized();
 }
 
-extern "C" JNIEXPORT jintArray JNICALL Java_io_github_aedancullen_triangular_system_NativePart_getPosition(JNIEnv*, jobject) {
+extern "C" JNIEXPORT jintArray JNICALL Java_io_github_aedancullen_triangular_NativePart_getPosition(JNIEnv*, jobject) {
     Rect rect =  tracker.getPosition();
     int array [4];
     array[0] = rect.x;
@@ -32,6 +32,6 @@ extern "C" JNIEXPORT jintArray JNICALL Java_io_github_aedancullen_triangular_sys
     return (jintArray)array;
 }
 
-extern "C" JNIEXPORT jint JNICALL Java_io_github_aedancullen_triangular_system_NativePart_getState(JNIEnv*, jobject) {
+extern "C" JNIEXPORT jint JNICALL Java_io_github_aedancullen_triangular_NativePart_getState(JNIEnv*, jobject) {
     return tracker.getState();
 }
